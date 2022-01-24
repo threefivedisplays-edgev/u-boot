32dc7269e9316b2b9ebfe00f78639f9421a5c795
threefivedisplays-edgev <98127956+threefivedisplays-edgev@users.noreply.github.com>
2022-01-24 15:04:11 -0700

    Update check-config.sh

--- scripts/check-config.sh
+++ scripts/check-config.sh
@@ -45,7 +45,7 @@ if [ -s ${new_adhoc} ]; then
 	echo >&2 "Please add these via Kconfig instead. Find a suitable Kconfig"
 	echo >&2 "file and add a 'config' or 'menuconfig' option."
 	# Don't delete the temporary files in case they are useful
-	exit 1
+#	exit 1
 else
 	rm ${suspects} ${ok} ${new_adhoc}
 fi
